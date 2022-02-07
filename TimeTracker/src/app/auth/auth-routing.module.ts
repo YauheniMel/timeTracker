428d5d405957e3163bbe1adab4082464518b5136
YauheniMel <207melnik@gmail.com>
2022-02-07 16:26:51 +0300

    feat: Create LoginPageComponent

--- TimeTracker/src/app/auth/auth-routing.module.ts
+++ TimeTracker/src/app/auth/auth-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 
-const routes: Routes = [{ path: '', component: RegisterPageComponent }];
+const routes: Routes = [{ path: '', component: LoginPageComponent }, { path: 'register', component: RegisterPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- TimeTracker/src/app/auth/login-page/login-page.component.ts
+++ TimeTracker/src/app/auth/login-page/login-page.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
+  authForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+  ) {}
 
   ngOnInit(): void {
+    this.authForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(8)]],
+    });
   }
 
+  onSubmit(): void {
+    if (this.authForm.invalid) {
+      alert('Form is not valid!');
+
+      return;
+    }
+
+    alert('Form submited!');
+  }
 }
