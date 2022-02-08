4e66c569e1307c3374d5722bc2c7cc284a34e41e
YauheniMel <207melnik@gmail.com>
2022-02-08 09:03:48 +0300

    feat: Add logout logic

--- TimeTracker/src/app/core/core.module.ts
+++ TimeTracker/src/app/core/core.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
 import { CoreRoutingModule } from './core-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
     CommonModule,
     CoreRoutingModule,
+    MatButtonModule,
   ],
 })
 export class CoreModule { }
