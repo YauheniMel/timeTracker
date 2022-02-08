bbde64e5af5042ab8da6ca85d9948f5b75cb5e26
YauheniMel <207melnik@gmail.com>
2022-02-08 13:09:36 +0300

    feat: add methods for setting database

--- TimeTracker/src/app/core/dashboard/dashboard.component.ts
+++ TimeTracker/src/app/core/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from './dashboard.service';
 
+interface User {
+  firstName: string;
+  lastName: string;
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  user!: User;
+
   constructor(private dashboardService: DashboardService) { }
 
   ngOnInit(): void {
+    this.dashboardService.getUserData().subscribe((response) => {
+      this.user = response.data();
+    });
   }
 
   logout(): void {
--- TimeTracker/src/app/core/dashboard/dashboard.service.ts
+++ TimeTracker/src/app/core/dashboard/dashboard.service.ts
@@ -1,13 +1,36 @@
 import { Injectable } from '@angular/core';
+import { getAuth } from '@angular/fire/auth';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
+import {
+  catchError, Observable, of, tap,
+} from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class DashboardService {
   constructor(
     private angularFireAuth: AngularFireAuth,
     private router: Router,
+    private db: AngularFirestore,
   ) { }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error:any): Observable<T> => {
+      console.error(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+
+  getUserData(): Observable<any> {
+    const user = getAuth().currentUser;
+
+    return this.db.doc(`users/${user?.uid}`).get().pipe(
+      tap((snapshot) => snapshot.data()),
+      catchError(this.handleError<any>('Get User')),
+    );
+  }
 
   logout(): void {
     this.angularFireAuth.signOut()
