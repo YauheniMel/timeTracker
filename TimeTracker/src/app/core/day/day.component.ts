117daf82cac6bf5643926fbaf91ab9a0c0d4f8cb
YauheniMel <207melnik@gmail.com>
2022-02-09 12:33:32 +0300

    feat: create day component

---
+++ TimeTracker/src/app/core/day/day.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-day',
+  templateUrl: './day.component.html',
+  styleUrls: ['./day.component.scss'],
+})
+export class DayComponent implements OnInit {
+  panelOpenState = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
