efc3433be3fbaf83aa984ee2521dd47a9213910c
YauheniMel <207melnik@gmail.com>
2022-02-10 13:43:49 +0300

    feat: add methods for change month value with pagination

--- TimeTracker/src/app/feature/components/calendar/calendar.component.ts
+++ TimeTracker/src/app/feature/components/calendar/calendar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { DateTime } from 'luxon';
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-calendar',
@@ -9,17 +9,43 @@ import { Component, OnInit } from '@angular/core';
 export class CalendarComponent implements OnInit {
   daysInMonth!: number[];
 
+  firstDayOfWeek!: number;
+
+  targetMonth: DateTime = DateTime.now();
+
   date!: Date;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.getDate();
+    this.date = new Date();
+
+    this.setDaysInMonth();
+    this.setFirstDay();
   }
 
-  getDate(): void {
-    this.daysInMonth = Array(31).fill(0).map((x, i) => i + 1);
+  setDaysInMonth(): void {
+    const countDays = this.targetMonth.daysInMonth;
+    this.daysInMonth = Array(countDays).fill(0).map((x, i) => i + 1);
+  }
+
+  setFirstDay() {
+    const { month } = this.targetMonth;
+    this.firstDayOfWeek = new Date(this.date.getFullYear(), month - 1, 1).getDay() + 1;
+  }
+
+  changeMonth(action: string) {
+    if (action === 'plus') {
+      this.targetMonth = this.targetMonth.plus({ month: 1 });
+    } else if (action === 'minus') {
+      this.targetMonth = this.targetMonth.minus({ month: 1 });
+    }
 
     this.date = new Date();
+    this.date.setFullYear(this.targetMonth.year);
+    this.date.setMonth(this.targetMonth.month - 1);
+
+    this.setDaysInMonth();
+    this.setFirstDay();
   }
 }
