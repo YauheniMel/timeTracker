3bda9e5a1db0b287182c0b2405a23350b2a48db2
YauheniMel <207melnik@gmail.com>
2022-02-23 18:35:10 +0300

    fix: unsubscribe after logout

--- TimeTracker/src/app/feature/components/dashboard/dashboard.component.ts
+++ TimeTracker/src/app/feature/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/core/auth/auth.service';
 import { DatabaseService } from 'src/app/core/database.service';
 import { User } from './user.interface';
@@ -8,18 +8,27 @@ import { User } from './user.interface';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
   user!: User;
 
-  constructor(private database: DatabaseService, private authService: AuthService) {}
+  subscribe!: any;
+
+  constructor(
+    private database: DatabaseService,
+    private authService: AuthService,
+  ) {}
 
   ngOnInit(): void {
-    this.database.getDbProfile().subscribe((user) => {
+    this.subscribe = this.database.getDbProfile().subscribe((user) => {
       this.user = user;
     });
   }
 
   logout(): void {
     this.authService.logout();
+  }
+
+  ngOnDestroy(): void {
+    this.subscribe.unsubscribe();
   }
 }
