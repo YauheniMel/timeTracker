69f38c7bc05f3bfa6a0aa5a877f37e9f7796c5a7
YauheniMel <207melnik@gmail.com>
2022-02-23 14:02:06 +0300

    refactor: Delete logic, which never use

--- TimeTracker/src/app/feature/components/services/dashboard.service.ts
+++ TimeTracker/src/app/feature/components/services/dashboard.service.ts
@@ -1,33 +1,6 @@
 import { Injectable } from '@angular/core';
-import { getAuth } from '@angular/fire/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import {
-  catchError, Observable, of, tap,
-} from 'rxjs';
 
 @Injectable()
 export class DashboardService {
-  constructor(
-    private db: AngularFirestore,
-  ) {}
-
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(`${operation} failed: ${error.message}`);
-
-      return of(result as T);
-    };
-  }
-
-  getUserData(): Observable<any> {
-    const user = getAuth().currentUser;
-
-    return this.db
-      .doc(`users/${user?.uid}`)
-      .get()
-      .pipe(
-        tap((snapshot) => snapshot.data()),
-        catchError(this.handleError<any>('Get User')),
-      );
-  }
+  constructor() {}
 }
